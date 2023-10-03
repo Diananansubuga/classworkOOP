0e74ace27b51980ca4ae61abde62c33c4d643895
DianaClarissa <115138008+DianaClarissa@users.noreply.github.com>
2023-10-03 14:40:36 +0300

    class work

---
+++ classes1.py
@@ -0,0 +1,11 @@
+class student():
+    regno="s22b23/009"
+    name="Diana"
+    age="21"
+
+# if __name__=='__main__':
+studentoject=student()
+studentobject2=student()
+
+print("student one is: ",studentoject.name)
+

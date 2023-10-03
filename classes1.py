e93a4a9ef38798f72240cdfffc3dc414b5f0099f
DianaClarissa <115138008+DianaClarissa@users.noreply.github.com>
2023-10-03 14:59:45 +0300

    3rd october

--- classes1.py
+++ classes1.py
@@ -2,7 +2,8 @@ class student():
     regno="s22b23/009"
     name="Diana"
     age="21"
-
+    def cal_age(self,yearOfBirth):
+        self.age=2023-yearOfBirth
 # if __name__=='__main__':
 studentoject=student()
 studentobject2=student()

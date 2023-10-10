2f45912f296fbcb62a1feba594c9043ca6441e39
DianaClarissa <117372394+Diananansubuga@users.noreply.github.com>
2023-10-10 15:15:29 +0300

    all

--- assignment2.py
+++ assignment2.py
@@ -1,1 +1,37 @@
 # write a procedual programme then convert it to an object oriented programme
+import tkinter as tk
+
+class Circle:
+    def __init__(self, center_x, center_y, radius):
+        self.center = [center_x, center_y]
+        self.radius = radius
+
+    def __repr__(self):
+        return f"Circle(center={self.center}, radius={self.radius})"
+
+    def draw(self, canvas):
+        rad = self.radius
+        x1 = self.center[0] - rad
+        x2 = self.center[1] - rad
+        y1 = self.center[0] + rad
+        y2 = self.center[1] + rad
+
+        canvas.create_oval(x1, x2, y1, y2, fill='green')
+
+    def move(self, x, y):
+        self.center = [x, y]
+
+# Create a Tkinter window
+window = tk.Tk()
+window.title("Circle Drawing")
+
+# Create a canvas to draw on
+canvas = tk.Canvas(window, width=400, height=400)
+canvas.pack()
+
+# Create a Circle object and draw it on the canvas
+circle = Circle(200, 200, 50)
+circle.draw(canvas)
+
+# Run the Tkinter main loop
+window.mainloop()
---
+++ circles.py
@@ -0,0 +1,38 @@
+
+
+import tkinter as tk
+class Circle(object):
+    def __init__(self,center,radius):
+        self.center=center
+        self.radius=radius
+
+    def draw(self,canvas):
+        rad=self.radius
+        x1=self.center[0]-rad
+        x2=self.center[1]-rad
+        y1=self.center[0]+rad
+        y2=self.center[1]+rad
+
+        canvas.create_oval(x1,x2,y1,y2, fill='red')
+    
+    def move(self,x,y):
+        self.center=[x,y]
+    
+window = tk.Tk()
+window.title("Circle Drawing")
+
+# Create a canvas to draw on
+canvas = tk.Canvas(window, width=400, height=400)
+canvas.pack()
+
+# Create a Circle object and draw it on the canvas
+circle = Circle( 200, 50)
+circle.draw(canvas)
+
+# Run the Tkinter main loop
+window.mainloop()
+
+
+
+
+
